7d3e7ecdbee67b2d102b726c3e1577af6c94df8c
Carol <carolbeninca94@gmail.com>
2026-03-19 11:20:14 -0300

    Atualizar layout da adega

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 produtos = []
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        tipo = request.form['tipo']
-        preco = request.form['preco']
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        tipo = request.form.get("tipo")
+        preco = request.form.get("preco")
 
         produtos.append({
-            'nome': nome,
-            'tipo': tipo,
-            'preco': preco
+            "nome": nome,
+            "tipo": tipo,
+            "preco": preco
         })
 
-        return redirect('/')
+    return render_template("index.html", produtos=produtos)
 
-    return render_template('index.html', produtos=produtos)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
