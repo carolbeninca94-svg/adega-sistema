c4f736ad86a5dac845a64e8824b24658c94af79b
Carol <carolbeninca94@gmail.com>
2026-03-19 12:20:53 -0300

    fix requiremens

--- app.py
+++ app.py
@@ -19,5 +19,8 @@ def index():
 
     return render_template("index.html", produtos=produtos)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
